eef891529bc6dcc3d654f8ba4581b286a2719f77
Taras Rodynenko <t.rodynenko@gmail.com>
2017-04-12 16:44:29 +0300

    add passport strategies (Facebook, Twitter)

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 const express = require("express");
 const app = express();
-const webpackMiddleware = require("webpack-dev-middleware");
-const webpack = require("webpack");
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
 
-const routes = require('./src/routes');
+const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET } = process.env;
 
-webpackConfig = {
-  entry: "./src/js/index.js",
-  output: {
-    path: '/src/public/',
-    library: "authLibrary"
+const routes = require('./src/routes');
+/*
+ * Init Facebook Strategy
+ */
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://b0c8a6a3.ngrok.io/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'photos', 'email']
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    console.log("logged by Facebook", profile)
+    cb()
   }
-};
+));
+
+/*
+ * Init Twitter Strategy
+ */
+ passport.use(new TwitterStrategy({
+     consumerKey: TWITTER_CONSUMER_KEY,
+     consumerSecret: TWITTER_CONSUMER_SECRET,
+     callbackURL: "http://b0c8a6a3.ngrok.io/auth/twitter/callback"
+   },
+   function(token, tokenSecret, profile, cb) {
+     console.log("logged by Twitter", profile)
+     cb()
+   }
+ ));
 
 app.use(webpackMiddleware(webpack(webpackConfig), {
   publicPath: "/src/public",
   lazy: true
 }));
 
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+
 app.set('port', process.env.port || 3000);
 app.use(express.static('src/public'));
 app.set('views', "./src/views");
 app.set('view engine', 'pug');
+
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 
