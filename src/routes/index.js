6908700f00088fa779494e8047ca0cced2a13db1
Taras Rodynenko <t.rodynenko@gmail.com>
2017-04-12 16:45:55 +0300

    add passport strategies routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/', (req,res) => {
   res.render('index');
 });
+
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+router.get('/auth/twitter', passport.authenticate('twitter'));
+
+router.get('/auth/twitter/callback',
+  passport.authenticate('twitter', { failureRedirect: '/' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 module.exports = router;
