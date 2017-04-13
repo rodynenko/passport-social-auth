06cda7ff2c1a05a61da2a85713177c17c9f8803f
Taras Rodynenko <t.rodynenko@gmail.com>
2017-04-13 12:03:02 +0300

    change src dir, modify npm commands and add foreman env

--- app.js
+++ app.js
@@ -3,50 +3,49 @@ const app = express();
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const TwitterStrategy = require('passport-twitter').Strategy;
+const routes = require('./routes');
 
 const { FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET } = process.env;
 
-const routes = require('./src/routes');
 /*
  * Init Facebook Strategy
  */
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://b0c8a6a3.ngrok.io/auth/facebook/callback",
-    profileFields: ['id', 'displayName', 'photos', 'email']
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log("logged by Facebook", profile)
-    cb()
-  }
-));
+if (FACEBOOK_APP_ID && FACEBOOK_APP_SECRET) {
+  passport.use(new FacebookStrategy({
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+      profileFields: ['id', 'displayName', 'photos', 'email']
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      console.log("logged by Facebook", profile)
+      cb()
+    }
+  ));
+}
 
 /*
  * Init Twitter Strategy
  */
- passport.use(new TwitterStrategy({
-     consumerKey: TWITTER_CONSUMER_KEY,
-     consumerSecret: TWITTER_CONSUMER_SECRET,
-     callbackURL: "http://b0c8a6a3.ngrok.io/auth/twitter/callback"
-   },
-   function(token, tokenSecret, profile, cb) {
-     console.log("logged by Twitter", profile)
-     cb()
-   }
- ));
-
-app.use(webpackMiddleware(webpack(webpackConfig), {
-  publicPath: "/src/public",
-  lazy: true
-}));
+if (TWITTER_CONSUMER_KEY && TWITTER_CONSUMER_SECRET) {
+  passport.use(new TwitterStrategy({
+      consumerKey: TWITTER_CONSUMER_KEY,
+      consumerSecret: TWITTER_CONSUMER_SECRET,
+      callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    function(token, tokenSecret, profile, cb) {
+      console.log("logged by Twitter", profile)
+      cb()
+    }
+  ));
+}
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 
 app.set('port', process.env.port || 3000);
 app.use(express.static('src/public'));
-app.set('views', "./src/views");
+app.set('views', "./views");
 app.set('view engine', 'pug');
 
 app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
